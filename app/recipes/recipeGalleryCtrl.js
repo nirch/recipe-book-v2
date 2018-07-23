384d10bc0a55f4c175fd4f6492f80a0bdb1c0c45
nirch <nirh2@yahoo.com>
2018-07-23 12:04:03 +0300

    reciped data coming from hard coded array

--- app/recipes/recipeGalleryCtrl.js
+++ app/recipes/recipeGalleryCtrl.js
@@ -9,4 +9,46 @@ app.controller('recipeGalleryCtrl', function($scope, user, $location) {
     $scope.test = "dkdkd";
 
     $scope.arr = [1,2,2,3,3,3,2,2,2,2,2,2,2,2,2,2,2];
+
+
+    $scope.recipes = [{
+        "id": 1,
+        "name": "Shakshuka",
+        "description": "An amazing egg dish",
+        "duration": 40,
+        "ingrediants": "Eggs, Tomattos, Garlic",
+        "steps": "Heat the pan, put the eggs",
+        "imageUrl": "https://www.onceuponachef.com/images/2017/06/Shakshuka_-2.jpg",
+        "userId": 1
+    },
+    {
+        "id": 2,
+        "name": "Shakshuka",
+        "description": "An amazing egg dish",
+        "duration": 40,
+        "ingrediants": "Eggs, Tomattos, Garlic",
+        "steps": "Heat the pan, put the eggs",
+        "imageUrl": "https://www.onceuponachef.com/images/2017/06/Shakshuka_-2.jpg",
+        "userId": 1
+    },
+    {
+        "id": 3,
+        "name": "Shakshuka",
+        "description": "An amazing egg dish",
+        "duration": 40,
+        "ingrediants": "Eggs, Tomattos, Garlic",
+        "steps": "Heat the pan, put the eggs",
+        "imageUrl": "https://www.onceuponachef.com/images/2017/06/Shakshuka_-2.jpg",
+        "userId": 1
+    },
+    {
+        "id": 4,
+        "name": "Shakshuka",
+        "description": "An amazing egg dish",
+        "duration": 40,
+        "ingrediants": "Eggs, Tomattos, Garlic",
+        "steps": "Heat the pan, put the eggs",
+        "imageUrl": "https://www.onceuponachef.com/images/2017/06/Shakshuka_-2.jpg",
+        "userId": 1
+    }];
 })
