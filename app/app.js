e641a11e5d194d038a9a6ebc9972b9b47e0e3314
nirch <nirh2@yahoo.com>
2018-07-23 10:10:13 +0300

    Adding login template + controller

--- app/app.js
+++ app/app.js
@@ -7,7 +7,8 @@ app.config(function($routeProvider) {
         templateUrl: "app/home/home.html"
     })
     .when("/login", {
-
+        templateUrl: "app/login/login.html",
+        controller: "loginCtrl"
     })
     .when("/signup", {
         
