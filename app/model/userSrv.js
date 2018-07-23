9fcc213e465104e423bc41061b6f74c09cf4f94f
nirch <nirh2@yahoo.com>
2018-07-23 11:31:02 +0300

    User service is now doing a "real" login

--- app/model/userSrv.js
+++ app/model/userSrv.js
@@ -21,12 +21,17 @@ app.factory('user', function($http, $q) {
     function login(email, password) {
         var async = $q.defer();
 
-        if (email === "nir@nir.com" && password === "123") {
-            activeUser = new User({fname:"Nir", lname:"Channes", email:email})
-            async.resolve(activeUser);
-        } else {
-            async.reject();
-        }
+        var loginURL = "http://my-json-server.typicode.com/nirch/recipe-book-v2/users?email=" + email + "&password=" + password;
+        $http.get(loginURL).then(function(response) {
+            if (response.data.length > 0) {
+                activeUser = new User(response.data[0]);
+                async.resolve(activeUser);
+            } else {
+                async.reject("invalid credentials");
+            }
+        }, function(err) {
+            async.reject(err);
+        });
 
         return async.promise;
     }
